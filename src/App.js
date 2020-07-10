f22d8a7aaeae163d16083f1a21feea4c17848861
Hamza Liaqat <hamzaliaqat1481@gmail.com>
2020-07-10 20:23:34 +0500

    react animated navbar using react GSAP library

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./app.css";
+
+import Header from "./components/Header";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <div className="wrapper">
+            <div className="home">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/About" component={About} />
+                <Route exact path="/Courses" component={Courses} />
+                <Route exact path="/contact" component={Contact} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <p>Discover our numerous About</p>;
+}
+
+function Courses() {
+  return <p>Courses that help you.</p>;
+}
+
+function Contact() {
+  return <p>Feel free to reach us.</p>;
+}
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h5>
+          The <b>HAMBRG</b>, is a creative, engineer driven, global agency
+          working on advancing the software, advertising and design communities
+          to new heights.
+        </h5>
+      </div>
+    </div>
+  );
+}
+export default App;
